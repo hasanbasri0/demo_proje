b40d2e89abc4de29f8aed061c856c58fcb7dc283
hasanbasri0 <67021744+hasanbasri0@users.noreply.github.com>
2020-06-20 23:10:24 +0300

    Update DemoClass.java
    ilk commit remote repostory'den

--- src/demo_package/DemoClass.java
+++ src/demo_package/DemoClass.java
@@ -4,6 +4,7 @@ public class DemoClass {
 
 	public static void main(String[] args) {
 		System.out.println("ilk commit local repository'den");
+		System.out.println("ilk commit remote repository'den");
 
 	}
 
